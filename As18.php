ba4d9ef0d6249ce1fce64957a151db244156bda9
ChristopherWashburn <81316479+ChristopherWashburn@users.noreply.github.com>
2021-04-07 11:42:20 -0400

    adding file to edit

---
+++ As18.php
@@ -0,0 +1,36 @@
+<?php
+echo "<a target='_blank' href='#'>GitHub Repo</a> ";
+
+main();
+
+function main () {
+	
+	$apiCall = 'http://api.covid19api.com/summary ';
+	$json_string = curl_get_contents($apiCall);
+	$obj = json_decode($json_string);
+	
+	$arr1 = Array();
+	$arr2 = Array();
+	foreach($obj->Countries as $i)
+	{
+		array_push($arr1, $i->Country );
+		array_push($arr2, $i->TotalDeaths );
+	}
+	
+	array_multisort($arr2, $arr1);
+	
+	print_r($arr1);
+	
+}
+
+function curl_get_contents($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
+?>
